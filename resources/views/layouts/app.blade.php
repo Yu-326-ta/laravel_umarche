d0107b2160717b91b06d40dcc8403fd911996d4e
yuta <yoshinagayuuta@outlook.jp>
2022-07-04 20:48:26 +0900

    日本語化対応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+                <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+                <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
