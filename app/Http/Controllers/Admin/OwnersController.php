bcb37ab344e4c1670a31c91863c706fb04f1d18d
yuta <yoshinagayuuta@outlook.jp>
2022-07-06 21:44:18 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -42,7 +42,7 @@ class OwnersController extends Controller
 
         // dd($e_all,$q_get,$q_first,$c_test);
 
-        $owners = Owner::select("name", "email", "created_at")->get();
+        $owners = Owner::select("id", "name", "email", "created_at")->get();
 
         return view("admin.owners.index",
         compact("owners"));
@@ -81,7 +81,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route("admin.owners.index")
-        ->with('message','オーナー登録を成功しました。');
+        ->with(['message'=>'オーナー登録を成功しました。',
+        'status' => 'info']);
 
     }
 
@@ -104,7 +105,9 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -116,7 +119,16 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -127,6 +139,21 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を削除しました。',
+        'status' => 'alert']);
+    }
+
+    public function expiredOwnerIndex() {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id) {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index')->with(['message'=>'オーナー情報を完全に削除しました', 'status' => 'alert']);;
     }
 }
