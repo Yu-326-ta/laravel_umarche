639abd6ebae43d26e5413d9aaf6c49ad03b5eae6
yuta <yoshinagayuuta@outlook.jp>
2022-07-06 22:52:27 +0900

    ルート、ビュー編集

--- routes/admin.php
+++ routes/admin.php
@@ -22,12 +22,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
 Route::resource('owners', OwnersController::class)
-->middleware('auth:admin');
+->middleware('auth:admin')->except(['show']);
 
 // 期限切れのオーナーの処理のURL
 Route::prefix('expired-owners')
