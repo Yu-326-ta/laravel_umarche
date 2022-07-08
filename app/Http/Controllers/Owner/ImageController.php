fc29ba9d124d5dcfe752985d7d7a9821de3e4c5c
yuta <yoshinagayuuta@outlook.jp>
2022-07-08 15:53:02 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -3,19 +3,38 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    //認証しているかのチェック
+    public function __construct()
     {
-        //
+        $this->middleware('auth:owners');
+
+        //shopControllerにのみ働くmiddlewareを定義
+        $this->middleware(function($request, $next) {
+            $id = $request->route()->parameter('image'); 
+            if(!is_null($id)) { 
+                $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId; //キャスト 文字列→数値
+                if ($imageId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        
+        });
     }
+
+public function index()
+{
+    $images = Image::where('owner_id', Auth::id())->orderBy('updated_at', 'desc')->paginate(20);
+
+    return view('owner.images.index', compact('images'));
+}
 
     /**
      * Show the form for creating a new resource.
