418bf1b440e4f46bdc66b5c4343342161aceb513
yuta <yoshinagayuuta@outlook.jp>
2022-07-08 13:10:35 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Owner;
  
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Shop;//69
+use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
  
 class ShopController extends Controller
 {
@@ -50,7 +51,15 @@ class ShopController extends Controller
     {
         $imageFile=$request->image;
         if(!is_null($imageFile) && $imageFile->isValid()){
-            Storage::putFile('public/shops', $imageFile);
+            // Storage::putFile('public/shops', $imageFile); リサイズなし
+
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName.'.'.$extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+
+            Storage::put('public/shops/'.$fileNameToStore, $resizedImage);
+
         }
 
         return redirect()->route('owner.shops.index');
