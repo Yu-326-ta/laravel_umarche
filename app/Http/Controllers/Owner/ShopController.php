9c81b6e7dec06b2e6711096d0200aef2eea208c4
yuta <yoshinagayuuta@outlook.jp>
2022-07-08 11:03:06 +0900

    middleware, abort(404)

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -1,33 +1,52 @@
 <?php
-
+ 
 namespace App\Http\Controllers\Owner;
-
+ 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Shop;
+use App\Models\Shop;//69
 use Illuminate\Support\Facades\Auth;
-
+ 
 class ShopController extends Controller
 {
+    //認証しているかのチェック
     public function __construct()
     {
-        $this->middleware('auth:owners');
-    } 
+        $this->middleware('auth:owners'); 
 
+
+        //shopControllerにのみ働くmiddlewareを定義
+        $this->middleware(function($request, $next) {
+            $id = $request->route()->parameter('shop'); //shopのid取得 [Route::get('edit/{shop}・・・')]の {shop}に該当
+            if(!is_null($id)) { 
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId; //キャスト 文字列→数値
+                $ownerId = Auth::id();
+                if ($shopId !== $ownerId) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+ 
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
-
+        //69
+        $ownerId = Auth::id(); // 認証されているid
+        $shops = Shop::where('owner_id', $ownerId)->get();// whereは検索条件
+ 
         return view('owner.shops.index',
         compact('shops'));
     }
-
+ 
     public function edit($id)
     {
+        //
     }
-
+ 
     public function update(Request $request, $id)
     {
+ 
     }
 }
