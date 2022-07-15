747b17a74d25e07b66a6dacc3b4a500e6d153d7f
yuta <yoshinagayuuta@outlook.jp>
2022-07-15 15:17:35 +0900

    route, itemcontroller, index

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             Categoryseeder::class,
             // ProductSeeder::class,
             // StockSeeder::class
+            UserSeeder::class,
         ]);
     }
 }
